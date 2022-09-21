6763658906eea832fa00297f6e675f94094c4c5b
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-21 19:59:12 -0300

    Prática if/else - exercicios 12, 13 e 14

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica-exercicio-12.js
@@ -0,0 +1,9 @@
+const a = 21;
+const b = 11;
+const c = 202;
+
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+  console.log(true)
+}else{
+  console.log(false);
+}
---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica-exercicio-13.js
@@ -0,0 +1,9 @@
+const a = 20;
+const b = 10;
+const c = 2021;
+
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+  console.log(true)
+}else{
+  console.log(false);
+}
