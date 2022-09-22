e0951753e7d97100b0d9cc4fd01105f61d6c0770
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-22 15:50:51 -0300

    pratica-exercio-1.js / 2º e 3º requisito

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/pratica-exercicio-1.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/pratica-exercicio-1.js
@@ -1,7 +1,19 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = null;
 
 // Percorra o array imprimindo todos os valores nele contidos com a função console.log()
 
 for (let index = 0; index < numbers.length; index++) {
   console.log(numbers[index]);
 }
+
+// Some todos os valores contidos no array e imprima o resultado
+
+for (let index = 0; index < numbers.length; index++) {
+  soma = soma + numbers[index];
+}
+console.log("Soma dos elementos do array: " + soma);
+
+// Calcule e imprima a média aritmética dos valores contidos no array
+
+console.log(soma / numbers.length);
