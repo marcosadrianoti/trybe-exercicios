dda7c969e36498315cd29e7d37424075ff22e8fb
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-22 15:13:32 -0300

    Exercicio teste de arrays

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-arrays.js
@@ -0,0 +1,24 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+// tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+
+// tasksList.shift();  // remove a última tarefa
+// console.log(tasksList);
+
+// let indexOfTask = tasksList.indexOf('Reunião');
+// console.log(indexOfTask);
+// __________________________________________________
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+menu.push('Contato');
+
+console.log(menuServices, indexOfPortfolio, menu);
