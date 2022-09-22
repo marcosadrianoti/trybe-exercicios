68b7cc4cd779e583a670dba60afcffb22f727a7f
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-22 18:20:04 -0300

    pratica-exercio-1.js / 8º requisito

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/pratica-exercicio-1.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/pratica-exercicio-1.js
@@ -62,3 +62,10 @@ for (let index = 0; index < numbers.length; index++) {
 }
 
 console.log("Menor valor no array:", menorValor);
+
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado
+let array25 = [];
+for (let index = 1; index <= 25; index+= 1) {
+  array25.push(index);
+}
+console.log(array25);
