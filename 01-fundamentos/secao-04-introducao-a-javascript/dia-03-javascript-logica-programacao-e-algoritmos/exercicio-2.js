6b45c6e68a36e644d9d719c45b796919e9c58952
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-23 16:15:11 -0300

    Palavra invertida

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-programacao-e-algoritmos/exercicio-2.js
@@ -0,0 +1,10 @@
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra
+
+let word = 'Marcos';
+let invertedWord = "";
+
+for (let index = word.length - 1; index >= 0; index--) {
+  invertedWord = invertedWord + word[index];
+}
+
+console.log(invertedWord);
