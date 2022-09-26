462be9628c1a1a08ece228d73579a7b804498278
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-26 18:27:41 -0300

    Incluido nova propriedade

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-1.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-1.js
@@ -5,3 +5,7 @@ let info = {
 };
 // Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 console.log("Bem-vinda,", info.personagem);
+
+// Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. 
+info.recorrente = "Sim";
+console.log(info);
