66fb5668baa92baffd63909dc6fd4a91c5e232b8
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-26 18:29:58 -0300

    Mostrando as chaves

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-1.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-1.js
@@ -9,3 +9,8 @@ console.log("Bem-vinda,", info.personagem);
 // Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. 
 info.recorrente = "Sim";
 console.log(info);
+
+// Faça um for/in que mostre todas as chaves do objeto. 
+for (let key in info) {
+  console.log(key);
+}
