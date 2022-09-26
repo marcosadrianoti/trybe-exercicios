42fb61bc13b35d9f0da54b3d126c8432f67b424b
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-26 18:50:06 -0300

    Ambos recorrentes

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-1.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-1.js
@@ -20,3 +20,19 @@ for (let key in info) {
 for (let key in info) {
   console.log(info[key]);
 }
+
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves.
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (const key in info) {
+  if (key != 'recorrente') {
+  console.log(info[key] + ' e ' + info2[key]);
+  }else {
+    console.log('Ambos recorrentes');
+  }
+}
