1d837c577add1f8304ea906d44cd91cba6b1330b
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-09-26 21:46:31 -0300

    Índice do maior valor do array

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-3.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-3.js
@@ -9,3 +9,20 @@ function verificaPalindrome(word) {
 }
 
 console.log(verificaPalindrome("arara"));
+
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor. Array de teste: [2, 3, 6, 7, 10, 1];
+let arrayTest = [2, 30, 6, 7, 10, 1];
+
+function indiceMaiorValor(array) {
+  let maiorValor = 0;
+  let indiceDoMaiorValor = null;
+  for (let key in array) {
+    if (array[key] > maiorValor){
+      maiorValor = array[key];
+      indiceDoMaiorValor = key;
+    }
+  }
+  return indiceDoMaiorValor;
+}
+
+console.log(indiceMaiorValor(arrayTest));
