70b1514ced6ba998b0a71d5c7a562d8e0b00882f
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-06 18:22:00 -0300

    Ajustes no exercício 5.4

--- 01-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 01-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -2,6 +2,11 @@ let mybody = document.querySelector('body');
 let page = document.getElementById('page');
 let inputBackgroundColor = document.getElementById('backgroundColor');
 let inputColor = document.getElementById('color');
+inputColor.value = JSON.stringify(
+  localStorage.getItem('backGroundColor'));
+  console.log(localStorage.getItem('backGroundColor'));
+  
+  // inputColor.value = rgb(238, 215, 215);
 
 let inputFontSize = document.getElementById('fontSize');
 inputFontSize.setAttribute('min', 8);
@@ -12,16 +17,20 @@ inputSpacelines.setAttribute('min', 8);
 inputSpacelines.setAttribute('max', 30);
 
 let fontFamily = document.getElementById('fontFamily');
+// Busquei informação como acrescentar option em: https://www.horadecodar.com.br/2021/08/03/como-adicionar-uma-option-a-um-select-com-javascript-puro/
 fontFamily.options[fontFamily.options.length] = new Option('Times New Roman', 'Times New Roman');
 fontFamily.options[fontFamily.options.length] = new Option('Arial', 'Arial');
 fontFamily.options[fontFamily.options.length] = new Option('Fantasy', 'Fantasy');
-// let optionFont = document.createElement('option');
-// optionFont.innerText('Times');
-// optionFont.setAttribute('value','Times');
-// console.log(optionFont.innerText);
-// fontFamily.appendChild('optionFont');
 
+// console.log(localStorage.getItem('fontFamily'));
 
+// if (localStorage.getItem('fontFamily' != '')){
+  mybody.style.backgroundColor = localStorage.getItem('backGroundColor');
+  mybody.style.color = localStorage.getItem('color');
+  page.style.fontSize = localStorage.getItem('fontSize');
+  page.style.lineHeight = localStorage.getItem('liheHeight');
+  page.style.fontFamily = localStorage.getItem('fontFamily');
+// }
 
 let btn = document.getElementById('btn-configure');
 btn.addEventListener('click', configPage);
@@ -33,6 +42,15 @@ function configPage() {
   page.style.fontSize = inputFontSize.value + 'px';
   page.style.lineHeight = inputSpacelines.value + 'px';
   page.style.fontFamily = fontFamily.value
-  
+
+  saveLocalStorage();
+}
+
+function saveLocalStorage(){
+  localStorage.setItem('backGroundColor', mybody.style.backgroundColor);
+  localStorage.setItem('color', mybody.style.color);
+  localStorage.setItem('fontSize', page.style.fontSize);
+  localStorage.setItem('liheHeight', page.style.lineHeight);
+  localStorage.setItem('fontFamily', page.style.fontFamily);
 }
 
