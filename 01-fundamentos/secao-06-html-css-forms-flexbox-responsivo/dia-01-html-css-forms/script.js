02db0673e43d4a5c64d72b10991fa2b6e0c735bb
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-06 20:44:46 -0300

    Autorização obrigatória do uso das imagens

--- 01-fundamentos/secao-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/script.js
+++ 01-fundamentos/secao-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/script.js
@@ -1,4 +1,8 @@
 let btnSubmit = document.getElementById('submit');
 btnSubmit.addEventListener('click', function(event){
   event.preventDefault();
+  let checkboxPicture = document.getElementById('my-pictures').checked;
+  if (checkboxPicture == false) {
+    alert('Autorize o uso das imagens antes de enviar seus dados.')
+  }
 })
