3681359887fda9b2143cf56fa2c40dcff60d8d9c
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-06 21:27:29 -0300

    Dados enviados com sucesso

--- 01-fundamentos/secao-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/script.js
+++ 01-fundamentos/secao-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/script.js
@@ -1,8 +1,15 @@
 let btnSubmit = document.getElementById('submit');
-btnSubmit.addEventListener('click', function(event){
+btnSubmit.addEventListener('click', function (event) {
   event.preventDefault();
   let checkboxPicture = document.getElementById('my-pictures').checked;
   if (checkboxPicture == false) {
     alert('Autorize o uso das imagens antes de enviar seus dados.')
+  }
+  let inputFullName = document.getElementById('full-name').value.length;
+  let inputEmail = document.getElementById('e-mail').value.length;
+  let textAreaReason = document.getElementById('longText').value.length;
+  if (inputFullName < 10 || inputFullName > 40 ||
+    inputEmail < 10 || inputEmail > 50 || textAreaReason > 500) {
+      alert('Dados Inválidos')
   }
 })
