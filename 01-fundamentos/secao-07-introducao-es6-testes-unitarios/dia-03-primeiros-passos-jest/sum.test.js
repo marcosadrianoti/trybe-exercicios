48de0f2144e7800f1293751d7a4513a0f060da97
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-19 20:52:41 -0300

    A chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado[1, 2, 3, 4]

--- 01-fundamentos/secao-07-introducao-es6-testes-unitarios/dia-03-primeiros-passos-jest/sum.test.js
+++ 01-fundamentos/secao-07-introducao-es6-testes-unitarios/dia-03-primeiros-passos-jest/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./sum');
+const { sum, myRemove } = require('./sum');
 
 describe('Função sum', () => {
   test('Teste se o retorno de sum(4, 5) é 9', () => {
@@ -8,17 +8,20 @@ describe('Função sum', () => {
     expect(sum(0, 0)).toEqual(0);
   });
   test('Lança um erro quando os parâmetros são 4 e "5"', () => {
-    expect(()=>{sum(4, '5')}).toThrow(Error);
+    expect(() => { sum(4, '5') }).toThrow(Error);
   });
   test('A mensagem de erro é “parameters must be numbers” quando realizar a chamada sum(4, "5")', () => {
-    expect(()=>{sum(4, '5')}).toThrow('parameters must be numbers');
+    expect(() => { sum(4, '5') }).toThrow('parameters must be numbers');
   });
 });
 describe('Função myRemove', () => {
-  test('a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+  test('A chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
-  test('a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+  test('A chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test('A chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
