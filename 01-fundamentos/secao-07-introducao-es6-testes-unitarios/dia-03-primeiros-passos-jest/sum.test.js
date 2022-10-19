26ba47e8944b8fc13cc1682ff90c4cd880721972
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-19 20:34:19 -0300

    Lança um erro quando os parâmetros são 4 e "5"

--- 01-fundamentos/secao-07-introducao-es6-testes-unitarios/dia-03-primeiros-passos-jest/sum.test.js
+++ 01-fundamentos/secao-07-introducao-es6-testes-unitarios/dia-03-primeiros-passos-jest/sum.test.js
@@ -4,4 +4,10 @@ describe('Função sum', () => {
   test('Teste se o retorno de sum(4, 5) é 9', () => {
     expect(sum(4, 5)).toEqual(9);
   });
+  test('Teste se o retorno de sum(0, 0) é 0', () => {
+    expect(sum(0, 0)).toEqual(0);
+  });
+  test('Lança um erro quando os parâmetros são 4 e "5"', () => {
+    expect(()=>{sum(4, '5')}).toThrow(Error);
+  });
 });
