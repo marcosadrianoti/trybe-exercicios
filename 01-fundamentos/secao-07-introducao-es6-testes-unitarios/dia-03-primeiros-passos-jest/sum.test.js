45954da70371a66e32f7d6dd6650e26a2a42adb4
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-19 20:53:13 -0300

    A chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado[1, 2, 3, 4] - correção

--- 01-fundamentos/secao-07-introducao-es6-testes-unitarios/dia-03-primeiros-passos-jest/sum.test.js
+++ 01-fundamentos/secao-07-introducao-es6-testes-unitarios/dia-03-primeiros-passos-jest/sum.test.js
@@ -21,7 +21,7 @@ describe('Função myRemove', () => {
   test('A chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
-  test('A chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+  test('A chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
