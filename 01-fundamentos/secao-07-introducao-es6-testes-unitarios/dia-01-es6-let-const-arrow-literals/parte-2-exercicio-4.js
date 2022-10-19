94a1c89edefe21fdd732db95489f0cad46e090db
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-19 12:21:58 -0300

    Parte 2 - execicio 4 - Concatenando frases

---
+++ 01-fundamentos/secao-07-introducao-es6-testes-unitarios/dia-01-es6-let-const-arrow-literals/parte-2-exercicio-4.js
@@ -0,0 +1,18 @@
+const substituaX = (name) => {
+  const frase = 'Tryber x aqui!';
+  let splitedFrase = frase.split(' ');
+  novaFrase = '';
+  for (let index = 0; index < splitedFrase.length; index++) {
+    if (splitedFrase[index] == 'x') {
+      splitedFrase[index] = name;
+    }
+  }
+  novaFrase = splitedFrase.join(' ');
+  return novaFrase;
+}
+
+const minhasSkills = (minhaFrase) => {
+  const skills = ['HTML', 'CSS', 'JavaScript'];
+  return finalFrase = `${minhaFrase}\nMinhas três principais habilidades são:\n${skills.join('\n')}`
+}
+console.log(minhasSkills(substituaX('Marcos'))); 
