2532ae447fe975e3dc2d1c1b0b7705234629b1af
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-26 15:01:42 -0300

    Seção 8 - dia 3 - Exercise1

--- 01-fundamentos/secao-08-higher-order-functions/dia-03-es6-hof-map-filter/exercise1.js
+++ 01-fundamentos/secao-08-higher-order-functions/dia-03-es6-hof-map-filter/exercise1.js
@@ -62,3 +62,18 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+// Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+// const expectedResult = [
+//   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+//   'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+//   'Fundação - Ficção Científica - Isaac Asimov',
+//   'Duna - Ficção Científica - Frank Herbert',
+//   'A Coisa - Terror - Stephen King',
+//   'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+// ];
+
+function formatedBookNames(books) {
+  return expectedResult = books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+  // return expectedResult;
+}
+console.log(formatedBookNames(books));
